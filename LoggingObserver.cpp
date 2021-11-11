f971abf1de783fe56cc2d01fb79a8476600f543a
Amine Halimi <35849317+kilner99@users.noreply.github.com>
2021-11-10 20:39:01 -0500

    Add files via upload

---
+++ LoggingObserver.cpp
@@ -0,0 +1,71 @@
+#include "LoggingObserver.h"
+#include <iostream>
+#include <list>
+#include <fstream>
+using std::list;
+using std::ofstream;
+using std::string;
+using std::endl;
+using std::cout;
+//ILoggable constructor. It is an interface
+ILoggable::ILoggable()
+{
+
+}
+//Subject class implementation
+//Constructor
+Subject::Subject()
+{
+    observers = new list<Observer*>;
+}
+Subject::~Subject()
+{
+    observers->clear();
+    delete observers;
+}
+//Necessary methods for a Subject
+//
+void Subject::Attach(Observer* o)
+{
+    observers->push_back(o);
+}
+void Subject::Detach(Observer* o)
+{
+    observers->remove(o);
+}
+void Subject::Notify(ILoggable* ILog)
+{
+    for(list<Observer*>::iterator it = observers->begin(); it != observers->end(); ++it)
+    {
+
+        (*it)->Update(ILog);
+    }
+}
+//Observer class implementation
+Observer::Observer()
+{
+
+}
+Observer::~Observer()
+{
+    //Does something
+}
+
+//LogObserver class Implementation
+LogObserver::LogObserver():Observer()
+{
+
+}
+LogObserver::~LogObserver()
+{
+
+}
+//Update method
+void LogObserver::Update(ILoggable* ILog)
+{
+    //Creates and output stream
+    ofstream output;
+    output.open("gamelog.txt");
+    output << ILog->stringToLog() << endl;
+    output.close();
+}
